2be972f0ce94feeb973cafe68598cfa3275ccac4
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-27 11:27:39 +0700

    Tambah Library iostream dan string serta namespace std

---
+++ Tugas.cpp
@@ -0,0 +1,6 @@
+//Buatlah sebuah program berisi 2 class dimana terdapat class layang-layang, belah ketupat'
+//Di setiap class nya berisikan luas dan keliling serta input dan output.
+//Setiap variable memiliki akses privat, BangunDatar belah ketupat merupakan friend function keliling dari layang-layang, dan
+//Semua class hanya dapat diakses menggunakan object.
+
+#include <iostream>
