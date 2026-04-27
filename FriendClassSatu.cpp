e0b808a56653a7f040b607c96102077555cd5452
risalahlele87-coder <risalahleles87@gmail.com>
2026-04-27 11:37:14 +0700

    Implementasi method setnama pada class orang dan implementasi method setid dan displayall pada class siswa

--- FriendClassSatu.cpp
+++ FriendClassSatu.cpp
@@ -25,3 +25,15 @@ public:
 
 };
 
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
